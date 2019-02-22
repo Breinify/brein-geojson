6914890afa74edf89da59e58ef8e9663304048f0
Todd Bodnar <todd.bodnar@breinify.com>
2019-02-21 16:06:48 -0800

    make a bounding box also be a geometry object

--- src/com/brein/geojson/geometry/BoundingBox.java
+++ src/com/brein/geojson/geometry/BoundingBox.java
@@ -2,10 +2,13 @@ package com.brein.geojson.geometry;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
-public class BoundingBox {
+public class BoundingBox implements IGeometryObject {
     private Point upRight;
     private Point downLeft;
+
+    private Polygon polyRepresentation = null;
 
     public BoundingBox(final Point initialPoint) {
         upRight = initialPoint;
@@ -33,6 +36,8 @@ public class BoundingBox {
             downLeft = new Point(Math.min(newPoint.getLat(), downLeft.getLat()),
                     Math.min(newPoint.getLon(), downLeft.getLon()));
         }
+
+        polyRepresentation = null;
     }
 
     public void addBBox(final BoundingBox other) {
@@ -52,8 +57,55 @@ public class BoundingBox {
         return downLeft;
     }
 
+    public Polygon getPolyRepresentation(){
+        if(polyRepresentation == null){
+            buildPoly();
+        }
+        return polyRepresentation;
+    }
+
+    protected void buildPoly(){
+        polyRepresentation = new Polygon(upRight, new Point(downLeft.getLon(), upRight.getLat()),
+                downLeft, new Point(upRight.getLon(), downLeft.getLat()));
+    }
+
     @Override
     public String toString() {
         return "[" + getDownLeft() + "," + getUpRight() + "]";
+    }
+
+    @Override
+    public boolean within(final IGeometryObject other) {
+        return getPolyRepresentation().within(other);
+    }
+
+    @Override
+    public boolean encases(final IGeometryObject other) {
+        return getPolyRepresentation().encases(other);
+    }
+
+    @Override
+    public double distance(final IGeometryObject other) {
+        return getPolyRepresentation().distance(other);
+    }
+
+    @Override
+    public double surfaceArea() {
+        return Math.abs((upRight.getLat() - downLeft.getLat()) * (upRight.getLon() - downLeft.getLon()));
+    }
+
+    @Override
+    public BoundingBox boundingBox() {
+        return this;
+    }
+
+    @Override
+    public Point centroid() {
+        return new Point((upRight.getLon() + downLeft.getLon()) / 2, (upRight.getLat() + downLeft.getLat()) / 2);
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        return getPolyRepresentation().toMap();
     }
 }
